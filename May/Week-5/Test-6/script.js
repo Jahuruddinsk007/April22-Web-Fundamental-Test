5f8d51632e89532de118a977990b1d73b6923260
JahirGithub <jahuruddinsk2000@gmail.com>
2022-05-29 19:16:55 +0530

    sunday test done

---
+++ May/Week-5/Test-6/script.js
@@ -0,0 +1,25 @@
+let num=1;
+switch (num) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+default:
+    console.log("Invalid input");
+}
---
+++ May/Week-5/test3.js
@@ -0,0 +1,9 @@
+let length = 10;
+let breadth = 20;
+
+if(length===breadth){
+    console.log("This is a square.")
+}
+else{
+    console.log("This is rectangle.")
+}
